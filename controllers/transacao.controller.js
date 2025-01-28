cbde51f6f08646f8ee6e4e10dc9a390d169ae837
Igor <igorcostalins2005@gmail.com>
2025-01-28 16:33:53 -0300

    Criação do controller /Transacao do tipo POST

--- controllers/transacao.controller.js
+++ controllers/transacao.controller.js
@@ -1,6 +1,34 @@
+const memoriaModel = require('../models/transacao.model');
+
 exports.adicionarTransacao = async (req, res) => {
-    console.log('Teste adicionar transacao.')
-}
+    try {
+        const dataAtual = new Date();
+
+        if (!req.body.valor || !req.body.dataHora) {
+            return res.status(422).end(); 
+        }
+
+        const dataTransacao = new Date(req.body.dataHora);
+        
+        if (dataAtual < dataTransacao) {
+            return res.status(422).end();
+        }
+        console.log(typeof req.body.valor)
+        if (typeof req.body.valor === 'number') {
+            if (req.body.valor < 0) {
+                return res.status(422).end();
+            }
+        } else {
+            return res.status(422).end();
+        }
+
+        await memoriaModel.criarTransacao(req.body);
+        return res.status(201).end();
+
+    } catch (error) {
+        return res.status(400).end();
+    }
+};
 
 exports.deletarTransacao = async (req, res) => {
     console.log('Teste delete transacao.')
---
+++ models/transacao.model.js
@@ -0,0 +1,12 @@
+let memoria = []
+
+exports.getMemoria = async () => {
+    return memoria; 
+}
+
+exports.criarTransacao = async (transacao) => {
+    memoria.push(transacao)
+    return transacao // Retornar a transação que foi inserida é uma boa prática, pois permite confirmar o que foi realmente adicionado ao array.
+};
+
+// Não expor diretamente a variável (module.exports = memoria) para garantir que somente essas funções acima possam edita-las. 
